dce14b439aef7b16656ab98aeab5f55c4eb3cbef
lakpashonasherpa <lakpashonasherpa@gmail.com>
2025-01-18 12:01:51 -0500

    updated the properties file

--- src/test/java/com/ddd/LibraryService/LibraryServiceApplicationTests.java
+++ src/test/java/com/ddd/LibraryService/LibraryServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.ddd.LibraryService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.test.ApplicationModuleTest;
 
-@SpringBootTest
+//@SpringBootTest
+//@DataJpaTest
+@ApplicationModuleTest
 class LibraryServiceApplicationTests {
 
 	@Test
